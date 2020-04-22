e0c3bb80ed8b7bd37670a2ca60d74f32c7905e69
arvincsh <63835623+arvincsh@users.noreply.github.com>
2020-04-22 18:35:02 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+var path = require("path");
+var fs = require("fs");
+var express =require("./node_modules/express");
+var app=express();
+var bodyParser = require('./node_modules/body-parser');
+var formidable = require('./node_modules/formidable');
+const child_process = require('child_process');
+var cv = require('./node_modules/opencv');
+var FormData = require('form-data');
+var url = require('url');
+var axios = require('axios');
+var pic = 2;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+app.get('/facedetection', function (req, res) {
+  var params = url.parse(req.url, true).query;
+		hostServer = params.url;
+    picnum= params.name;
+    console.log(picnum);
+    picfps= params.fpsrate;
+    console.log(picfps);
+
+    var form = new FormData();
+    		form.append('upload', fs.readFileSync(__dirname+"/public/extract/"+picfps+"fps/test1_"+picnum+".jpg"),"test1_"+picnum+".jpg");
+    		var formHeaders = form.getHeaders();
+
+    		var start_T = new Date().getTime();
+        //console.log("http://"+hostServer+"/face");
+    		axios.post("http://"+hostServer+"/face", form, {
+    		  headers:formHeaders,
+    		}).then(function (response){
+    			var end_T = new Date().getTime();
+    			cost = end_T-start_T;
+          console.log(cost);
+    			res.write(cost+":");
+    			res.write(response.data+":");
+          res.write("success");
+    			res.end();
+          //console.log(response.data);
+    		}).catch(function (error){
+          console.log("err");
+    			var end_T = new Date().getTime();
+    			cost = end_T-start_T;
+    			res.write(cost+":");
+    			res.write("fail"+":");
+          res.write("fail"+":");
+    			res.end();
+    		})
+})
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+})
