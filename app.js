d7f4345368527145c9dededebda84ecd6e6c0dec
arvincsh <63835623+arvincsh@users.noreply.github.com>
2020-05-05 01:38:04 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -19,45 +19,44 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
   res.render('index');
 })
-app.get('/facedetection', function (req, res) {
-  //console.log("facedetection");
-  var params = url.parse(req.url, true).query;
-		hostServer = params.url;
-  //  console.log(hostServer);
-    picnum= params.name;
-    //console.log(picnum);
-    picfps= params.fpsrate;
-    picdes= params.destiation;
-    //console.log(picdes);
+app.get('/textdetection', function (req, res) {
+  //console.log("textdetection");
+  res.setHeader("Access-Control-Allow-Origin", "*");  // 设置可访问的源
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    var form = new FormData();
-    		form.append('upload', fs.readFileSync(__dirname+"/public/extract/"+picfps+"fps/test1_"+picnum+".jpg"),"test1_"+picnum+".jpg");
-    		var formHeaders = form.getHeaders();
-    		var start_T = new Date().getTime();
-      //  console.log("http://"+hostServer+"/"+picdes);
-    		//axios.post("http://"+hostServer+"/"+picdes, form, {
-        axios.post("http://"+hostServer+"/"+picdes, form, {
-    		  headers:formHeaders,
-    		}).then(function (response){
-          //console.log("success");
-    			var end_T = new Date().getTime();
-    			cost = end_T-start_T;
-          //console.log(cost);
-    			res.write(cost+":");
-    			res.write(response.data);
-    			res.end();
-      //    console.log(response.data);
-    		}).catch(function (error){
-          //console.log("fail");
-    			var end_T = new Date().getTime();
-    			cost = end_T-start_T;
-    			res.write(cost+":");
-    			res.write("fail");
-    			res.end();
-          //console.log(response.data);
-    		})
+  // 解析 url 参数
+  var params = url.parse(req.url, true).query;
+  hostServer = params.url;
+  picnum=params.name;
+  dest=params.destiation;
+
+ //console.log(hostServer);
+ //console.log(picnum);
+ //console.log(__dirname+"/public/yzm/"+picnum+".png");
+  var form = new FormData();
+  form.append('upload', fs.readFileSync(__dirname+"/public/yzm/"+picnum+".png"), "yzm/"+picnum+".png");
+  var formHeaders = form.getHeaders();
+  var start_T = new Date().getTime();
+  axios.post("http://"+hostServer+"/"+dest, form, {
+    headers:formHeaders,
+  }).then(function (response){
+    //console.log("success");
+    var end_T = new Date().getTime();
+    cost = end_T-start_T;
+    res.write(cost+":");
+    res.write(response.data);
+    res.end();
+    //console.log(response.data);
+  }).catch(function (error){
+    //console.log("fail");
+    var end_T = new Date().getTime();
+    cost = end_T-start_T;
+    res.write(cost+":");
+    res.write("fail");
+    res.end();
+  })
 })
 
-app.listen(3000, function () {
-  console.log('app listening on port 3000!')
+app.listen(3001, function () {
+  console.log('app listening on port 3001!')
 })
